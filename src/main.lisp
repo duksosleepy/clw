e5959653d125379758dc3693c642f1e820506f8d
Song Khoi <songkhoi123@gmail.com>
2025-02-08 23:50:56 +0700

    add route

--- src/main.lisp
+++ src/main.lisp
@@ -13,11 +13,20 @@
 <body>
   <ul>
   {% for product in products %}
-    <li> {{ product.1 }} - {{ product.2 }} </li>
+    <li>
+      <a href=\"/product/{{ product.0 }}\">{{ product.1 }} - {{ product.2 }}</a>
+    </li>
   {% endfor %}
  </ul>
 </body>
 ")
+
+(defparameter *template-product* "
+<body>
+     {{ product }}
+</body>
+")
+
 
 (defun products (&optional (n 5))
   (loop for i from 0 below n
@@ -25,14 +34,35 @@
                       (format nil "Product nb ~a" i)
                       9.99)))
 
+(defun get-product (n)
+  ;; Query the DB.
+  (list n (format nil "Product nb ~a" n) 9.99))
+
 (defun render-products ()
   (djula:render-template*
    (djula:compile-string *template-root*)
    nil
    :products (products)))
 
+
+(defun render-product (n)
+  (djula:render-template*
+   (djula:compile-string *template-product*)
+   nil
+   :product (get-product n)))
+
+(defun render (template &rest args)
+  (apply
+      #'djula:render-template*
+      (djula:compile-string template)
+    nil
+    args))
+
 (easy-routes:defroute root ("/") ()
                       (render-products))
+
+(easy-routes:defroute product-route ("/product/:n") (&path (n 'integer))
+                      (format nil "parameter is: ~a and is of type: ~a" n (type-of n)))
 
 (defun start-server (&key (port *port*))
   (format t "~&Starting the web server on port ~a~&" port)
