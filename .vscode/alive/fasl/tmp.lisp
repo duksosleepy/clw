82fbae5d79fe4deb8fee7ddbe59502afd76ddf55
Song Khoi <songkhoi123@gmail.com>
2025-02-24 20:55:14 +0700

    refactor route

--- .vscode/alive/fasl/tmp.lisp
+++ .vscode/alive/fasl/tmp.lisp
@@ -4,117 +4,87 @@
   (:export :start-server))
 (in-package #:clw)
 
+(djula:add-template-directory (asdf:system-relative-pathname "clw" "templates/"))
+
+(defparameter +base.html+ (djula:compile-template* "base.html"))
+
+(defparameter +products.html+ (djula:compile-template* "products.html"))
+
+(defparameter +product.html+ (djula:compile-template* "product.html"))
 
 (defparameter *port* 4444 "The application port.")
 
-
-(defparameter *template-root* "
-
-<html>
-<head>
-  <link
-  rel=\"stylesheet\"
-  href=\"https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css\">
-</head>
-
-<body class=\"container\">
- <form action=\"/\" method=\"GET\">
-  <div>
-    <label for=\"query\">What do you search for?</label>
-    <input name=\"query\" id=\"query\" placeholder=\"Search…\" />
-  </div>
-  <div>
-    <button>Search</button>
-  </div>
-</form>
-
-{% if query %}
-<div> query is: {{ query }} </div>
-
-<ul>
-  {% for product in results %}
-    <li>
-      <a href=\"/product/{{ product.0 }}\">{{ product.1 }} - {{ product.2 }}</a>
-    </li>
-  {% endfor %}
-</ul>
-{% endif %}
-</body>
-</html>
-")
-
-(defparameter *template-product* "
-<body>
-     {{ product }}
-
-{% if debug %} debug info! {% endif %}
-</body>
-")
-
-
 (defun products (&optional (n 5))
-  (loop for i from 0 below n
-        collect (get-product i)))
+       (loop for i from 0 below n
+             collect (get-product i)))
+
 
 (defun get-product (n)
-  ;; Query the DB.
-  (list n (format nil "Product nb ~r" n) 9.99))
+       ;; Query the DB.
+       (list :id n :name (format nil "Product nb ~r" n) :price 9.99))
 
 (defun search-products (products query)
-  (loop for product in products
-          if (search query (second product) :test #'equalp)
-        collect product))
+       (loop for product in products
+               if (search query (second product) :test #'equalp)
+             collect product))
 
-(defun render-products ()
-  (djula:render-template*
-   (djula:compile-string *template-root*)
-   nil
-   :products (products)))
+; (defun render-products ()
+;        (djula:render-template*
+;         (djula:compile-string *template-root*)
+;         nil
+;         :products (products)))
 
+; (defun render-product (n)
+;        (djula:render-template*
+;         (djula:compile-string *template-product*)
+;         nil
+;         :product (get-product n)))
 
-(defun render-product (n)
-  (djula:render-template*
-   (djula:compile-string *template-product*)
-   nil
-   :product (get-product n)))
+; (defun render (template &rest args)
+;        (apply
+;         #'djula:render-template*
+;         (djula:compile-string template)
+;         nil
+;         args))
 
-(defun render (template &rest args)
-  (apply
-      #'djula:render-template*
-      (djula:compile-string template)
-    nil
-    args))
+; (def-filter :myfilter-name (var arg)
+;             (body))
 
+; (def-filter :add (it n)
+;             (+ it (parse-integer n)))
 
-(easy-routes:defroute root ("/") (query)
-                      (render *template-root*
-                              :results (search-products (products) query)
-                              :query query))
+(easy-routes:defroute root ("/" :method :get) ()
+                      (djula:render-template* +products.html+
+                                              nil
+                                              :products (products)))
 
-(easy-routes:defroute hello-route ("/hello") () "hello new route")
+(easy-routes:defroute hello-route ("/hello" :method :get) () "hello new route")
 
-(easy-routes:defroute product-route ("/product/:n") (&get debug &path (n 'integer))
-                      (render *template-product*
-                              :product (get-product n)
-                              :debug debug))
+(easy-routes:defroute product-route ("/product/:n" :method :get) (&path (n 'integer))
+                      (djula:render-template* +product.html+
+                                              nil
+                                              :product (get-product n)))
+
+(easy-routes:defroute name ("/tasks/:id" :method :get) (debug &get z)
+                      (format nil "we want the task of id: ~a with parameters debug: ~a and z: ~a" id debug z))
 
 (defun serve-static-assets ()
-  "Then reference static assets with the /static/ URL prefix."
-  (push (hunchentoot:create-folder-dispatcher-and-handler
-         "/static/"
-         (asdf:system-relative-pathname :clw "src/static/"))
-        ;;                                        ^^^ starts without a /
-        hunchentoot:*dispatch-table*))
+       "Then reference static assets with the /static/ URL prefix."
+       (push (hunchentoot:create-folder-dispatcher-and-handler
+              "/static/"
+              (asdf:system-relative-pathname :clw "src/static/"))
+             ;;                                        ^^^ starts without a /
+             hunchentoot:*dispatch-table*))
 
 (defun start-server (&key (port *port*))
-  (format t "~&Starting the web server on port ~a~&" port)
-  (force-output)
-  (setf *server* (make-instance 'easy-routes:easy-routes-acceptor
-                   :port (or *port* 4444)
-                   :document-root #p"www/"))
-  (serve-static-assets)
-  (hunchentoot:start *server*))
+       (format t "~&Starting the web server on port ~a~&" port)
+       (force-output)
+       (setf *server* (make-instance 'easy-routes:easy-routes-acceptor
+                                     :port (or *port* 4444)
+                                     :document-root #p"www/"))
+       (serve-static-assets)
+       (hunchentoot:start *server*))
 
 (defun main ()
-  (start-server :port (find-port:find-port :min *port*))
-  (sleep most-positive-fixnum))
+       (start-server :port (find-port:find-port :min *port*))
+       (sleep most-positive-fixnum))
